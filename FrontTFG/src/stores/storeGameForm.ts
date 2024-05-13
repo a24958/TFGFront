faf463e3be50b18c3b4312b7c203f0ca069eb918
a24958 <a24958@svalero.com>
2024-05-14 00:21:34 +0200

    Create gameFormStore for all the functions of the game form

---
+++ FrontTFG/src/stores/storeGameForm.ts
@@ -0,0 +1,10 @@
+import { defineStore } from "pinia";
+
+export const gameFormStore = defineStore('gameFormFunctions', () => {
+    // const load = (loadig: boolean) => {
+    //     loading.value = true;
+    //     setTimeout(() => {
+    //         loading.value = false;
+    //     }, 2000);
+    // };
+})
