490c38701a9646d0efc0656247b63dcdd5111c21
a24958 <a24958@svalero.com>
2024-05-16 21:11:48 +0200

    Apply the first pasapalbra styles

--- FrontTFG/src/components/Pasapalabra.vue
+++ FrontTFG/src/components/Pasapalabra.vue
@@ -103,17 +103,171 @@ const handleInputChangeDebounced = debounce((value: string) => {
 
 <template>
     <div>
-        <p>{{ id }}</p>
         <p> {{ name }}</p>
         <br>
-        <PreguntaPasapalabra :id="preguntas[idFirstQuestion].id" :pregunta="preguntas[idFirstQuestion].pregunta"
-            :respuesta="preguntas[idFirstQuestion].respuesta" :letra="preguntas[idFirstQuestion].letra"
-            :contestado="preguntas[idFirstQuestion].contestado" :acertado="preguntas[idFirstQuestion].acertado"
-            v-if="preguntas[idFirstQuestion].contestado === false">
-        </PreguntaPasapalabra>
-        <input type="text" @input="handleInputChange" @keyup.enter="functionTestEnter(inputValue)" v-model="inputValue">
-        <button @click="nextQuestion">Pasapalabra</button>
+        <div class="game-container">
+            <div id="rosco-container">
+                <ul id="rosco">
+                    <li v-for="element in props.preguntas">
+                        <PreguntaPasapalabra :id="element.id" :pregunta="element.pregunta"
+                            :respuesta="element.respuesta" :letra="element.letra" :contestado="element.contestado"
+                            :acertado="element.acertado">
+                        </PreguntaPasapalabra>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="inputs">
+            <input type="text" @input="handleInputChange" @keyup.enter="functionTestEnter(inputValue)"
+                v-model="inputValue">
+            <button @click="nextQuestion">Pasapalabra</button>
+        </div>
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.inputs {
+    position: absolute;
+    left: 42%;
+    bottom: 5%;
+}
+
+.game-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+#rosco-container {
+    position: relative;
+    width: 500px;
+    height: 500px;
+}
+
+#rosco {
+    list-style-type: none;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+}
+
+#rosco li {
+    position: absolute;
+}
+
+#rosco li.correct {
+    background-color: #8eff8e;
+}
+
+#rosco li.incorrect {
+    background-color: #ff8e8e;
+}
+
+#rosco li:nth-child(1) {
+    transform: rotate(-90deg) translate(250px) rotate(90deg);
+}
+
+#rosco li:nth-child(2) {
+    transform: rotate(-76.15deg) translate(250px) rotate(76.15deg);
+}
+
+#rosco li:nth-child(3) {
+    transform: rotate(-62.3deg) translate(250px) rotate(62.3deg);
+}
+
+#rosco li:nth-child(4) {
+    transform: rotate(-48.45deg) translate(250px) rotate(48.45deg);
+}
+
+#rosco li:nth-child(5) {
+    transform: rotate(-34.6deg) translate(250px) rotate(34.6deg);
+}
+
+#rosco li:nth-child(6) {
+    transform: rotate(-20.75deg) translate(250px) rotate(20.75deg);
+}
+
+#rosco li:nth-child(7) {
+    transform: rotate(-6.9deg) translate(250px) rotate(6.9deg);
+}
+
+#rosco li:nth-child(8) {
+    transform: rotate(6.95deg) translate(250px) rotate(-6.95deg);
+}
+
+#rosco li:nth-child(9) {
+    transform: rotate(20.8deg) translate(250px) rotate(-20.8deg);
+}
+
+#rosco li:nth-child(10) {
+    transform: rotate(34.65deg) translate(250px) rotate(-34.65deg);
+}
+
+#rosco li:nth-child(11) {
+    transform: rotate(48.5deg) translate(250px) rotate(-48.5deg);
+}
+
+#rosco li:nth-child(12) {
+    transform: rotate(62.35deg) translate(250px) rotate(-62.35deg);
+}
+
+#rosco li:nth-child(13) {
+    transform: rotate(76.2deg) translate(250px) rotate(-76.2deg);
+}
+
+#rosco li:nth-child(14) {
+    transform: rotate(90.05deg) translate(250px) rotate(-90.05deg);
+}
+
+#rosco li:nth-child(15) {
+    transform: rotate(103.9deg) translate(250px) rotate(-103.9deg);
+}
+
+#rosco li:nth-child(16) {
+    transform: rotate(117.75deg) translate(250px) rotate(-117.75deg);
+}
+
+#rosco li:nth-child(17) {
+    transform: rotate(131.6deg) translate(250px) rotate(-131.6deg);
+}
+
+#rosco li:nth-child(18) {
+    transform: rotate(145.45deg) translate(250px) rotate(-145.45deg);
+}
+
+#rosco li:nth-child(19) {
+    transform: rotate(159.3deg) translate(250px) rotate(-159.3deg);
+}
+
+#rosco li:nth-child(20) {
+    transform: rotate(173.15deg) translate(250px) rotate(-173.15deg);
+}
+
+#rosco li:nth-child(21) {
+    transform: rotate(187deg) translate(250px) rotate(-187deg);
+}
+
+#rosco li:nth-child(22) {
+    transform: rotate(200.85deg) translate(250px) rotate(-200.85deg);
+}
+
+#rosco li:nth-child(23) {
+    transform: rotate(214.7deg) translate(250px) rotate(-214.7deg);
+}
+
+#rosco li:nth-child(24) {
+    transform: rotate(228.55deg) translate(250px) rotate(-228.55deg);
+}
+
+#rosco li:nth-child(25) {
+    transform: rotate(242.4deg) translate(250px) rotate(-242.4deg);
+}
+
+#rosco li:nth-child(26) {
+    transform: rotate(256.25deg) translate(250px) rotate(-256.25deg);
+}
+</style>
--- FrontTFG/src/components/PreguntaPasapalabra.vue
+++ FrontTFG/src/components/PreguntaPasapalabra.vue
@@ -12,9 +12,22 @@ const props = defineProps<{
 <template>
     <div>
         <p>{{ letra }}</p>
-        <p>{{ pregunta }}</p>
-        <p>{{ respuesta }}</p>
+        <!-- <p>{{ pregunta }}</p>
+        <p>{{ respuesta }}</p> -->
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+div {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    border: 2px solid white;
+    height: 38px;
+    width: 38px;
+    border-radius: 50%;
+    background-color: rgb(0, 123, 255);
+    color: white;
+}
+</style>
